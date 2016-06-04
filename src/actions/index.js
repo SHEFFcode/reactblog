a2b2a60acacc0dd48dc9caa4f9424a12fb6e7638
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-04 14:20:30 -0700

    add create post action handler and mapDispatchToProps it into the PostsNew component using redux form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=sheffmachine';
 
@@ -10,4 +11,12 @@ export function fetchPosts () {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export function createPost (posts) {
+  const request = axios.post(`${ ROOT_URL }/posts${ API_KEY }`, posts);
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
-    return (
-      <form action="">
+    const { fields: { title, categories, content }, handleSubmit } = this.props; // same as const handleSubmit = this.props.handleSubmit;
+    return (                                                                     // same as const title = this.props.fields.title;
+      <form onSubmit={ handleSubmit(this.props.createPost) }>
         <h3>Create a new post</h3>
         <div className="form-group">
           <label htmlFor="title">Title</label>
-          <input type="text" className="form-control"/>
+          <input type="text" className="form-control" { ...title }/>
         </div>
         <div className="form-group">
           <label htmlFor="categories">Categories</label>
-          <input type="text" className="form-control"/>
+          <input type="text" className="form-control" { ...categories }/>
         </div>
         <div className="form-group">
           <label htmlFor="content">Content</label>
-          <textarea name="" id="" cols="30" rows="10" className="form-control" />
+          <textarea name="" id="" cols="30" rows="10" className="form-control" { ...content } />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
@@ -24,7 +26,10 @@ class PostsNew extends Component {
   }
 }
 
+//connect: first argument is mapStateToProps, 2d mapDispatchToProps
+// reduxForm: first is formConfig, 2d is mapStateToProps, 3d is mapDispatchToProps
+
 export default reduxForm({
   form: 'PostsNew',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
