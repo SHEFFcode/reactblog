67bc8bf6aa4399a101dea37dc4712fe07eb35c2b
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-04 16:16:12 -0700

    add in the show posts component, hook up the show posts action and connect it to the component

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -7,12 +7,27 @@ class PostsIndex extends Component {
   componentWillMount() {
     this.props.fetchPosts();
   }
+  renderPosts () {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={ post.id }>
+          <Link to={ "posts/" + post.id }>
+            <span className="pull-xs-right">{ post.categories }</span>
+            <strong>{ post.title }</strong>
+          </Link>
+        </li>);
+    });
+  }
   render() {
     return (
       <div>
         <div className="text-xs-right">
           <Link to="/posts/new" className="btn btn-primary">Add a post</Link>
         </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          { this.renderPosts() }
+        </ul>
       </div>
     );
   }
@@ -22,5 +37,9 @@ class PostsIndex extends Component {
 //   return bindActionCreators({ fetchPosts }, dispatch);
 // }
 
-export default connect(null, { fetchPosts } )(PostsIndex);// This is a little shortcut without the
+function mapStateToProps (state) {
+  return { posts: state.posts.all }
+}
+
+export default connect(mapStateToProps, { fetchPosts } )(PostsIndex);// This is a little shortcut without the
 //                                                                        mapDispatchToProps function.
